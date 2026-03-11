89faad55c024caa7b334b90436597da35a9782f6
rathipriya-code <rathipriya@jsconsulting.in>
2026-03-11 03:40:25 +0000

    CHAPTER-2.py File is modified and Completed

--- CHAPTER-2.py
+++ CHAPTER-2.py
@@ -9,29 +9,29 @@ DAY01-Task
 Single Inheritance
 
 '''
-class Cus_1:
-    def Apartment_flat1(self):
-        print("===================CUSTOMER_1============================")
+class Apartment_1:
+    def display_customer_1(self):
+        print("===================APARTMENT_1============================")
         print("Priya bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat2(self):
-        print("Priya bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat3(self):
-        print("Priya bought 3-BHK-APARTMENT with price of RS.70,00,000")
-class Cus_2(Cus_1):
-    def Apartment_flat4(self):
-        print("===================CUSTOMER_2============================")
-        print("Sreya bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat5(self):
-        print("Sreya bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat6(self):
-        print("Sreya bought 3-BHK-APARTMENT with price of RS.70,00,000")
-Price_details=Cus_2()
-Price_details.Apartment_flat1()
-Price_details.Apartment_flat2()
-Price_details.Apartment_flat3()
-Price_details.Apartment_flat4()
-Price_details.Apartment_flat5()
-Price_details.Apartment_flat6()
+    def display_customer_2(self):
+        print("sreya bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_3(self):
+        print("shivani bought 3-BHK-APARTMENT with price of RS.70,00,000")
+class Apartment_2(Apartment_1):
+    def display_customer_4(self):
+        print("===================APARTMENT_2============================")
+        print("Harish bought 1-BHK-APARTMENT with price of RS.30,00,000")
+    def display_customer_5(self):
+        print("Sayesha bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_6(self):
+        print("Sri bought 3-BHK-APARTMENT with price of RS.70,00,000")
+Price_details=Apartment_2()
+Price_details.display_customer_1()
+Price_details.display_customer_2()
+Price_details.display_customer_3()
+Price_details.display_customer_4()
+Price_details.display_customer_5()
+Price_details.display_customer_6()
 print()
 print("====================SINGLE INHERITANCE=====================")
 print()
@@ -41,40 +41,40 @@ print()
 Multiple Inheritance
 
 '''
-class Cus_1:
-    def Apartment_flat1(self):
-        print("===================CUSTOMER_1============================")
-        print("Priya bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat2(self):
-        print("Priya bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat3(self):
+class Apartment_1:
+    def display_customer_1(self):
+        print("===================APARTMENT_1============================")
+        print("simbu bought 1-BHK-APARTMENT with price of RS.30,00,000")
+    def display_customer_2(self):
+        print("Preethi bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_3(self):
         print("Priya bought 3-BHK-APARTMENT with price of RS.70,00,000")
-class Cus_2():
-    def Apartment_flat4(self):
-        print("===================CUSTOMER_2============================")
-        print("Sreya bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat5(self):
+class Apartment_2():
+    def display_customer_4(self):
+        print("===================APARTMENT_2============================")
+        print("Anu bought 1-BHK-APARTMENT with price of RS.30,00,000")
+    def display_customer_5(self):
         print("Sreya bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat6(self):
-        print("Sreya bought 3-BHK-APARTMENT with price of RS.70,00,000")
-class Cus_3(Cus_1,Cus_2):
-    def Apartment_flat7(self):
-        print("===================CUSTOMER_3============================")
-        print("Bala bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat8(self):
+    def display_customer_6(self):
+        print("Sri bought 3-BHK-APARTMENT with price of RS.70,00,000")
+class Apartment_3(Apartment_1,Apartment_2):
+    def display_customer_7(self):
+        print("===================APARTMENT_3============================")
+        print("Gayatri bought 1-BHK-APARTMENT with price of RS.30,00,000")
+    def display_customer_8(self):
         print("Bala bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat9(self):
-        print("Bala bought 3-BHK-APARTMENT with price of RS.70,00,000")
-Price_details=Cus_3()
-Price_details.Apartment_flat1()
-Price_details.Apartment_flat2()
-Price_details.Apartment_flat3()
-Price_details.Apartment_flat4()
-Price_details.Apartment_flat5()
-Price_details.Apartment_flat6()
-Price_details.Apartment_flat7()
-Price_details.Apartment_flat8()
-Price_details.Apartment_flat9()
+    def display_customer_9(self):
+        print("Bindhu bought 3-BHK-APARTMENT with price of RS.70,00,000")
+Price_details=Apartment_3()
+Price_details.display_customer_1()
+Price_details.display_customer_2()
+Price_details.display_customer_3()
+Price_details.display_customer_4()
+Price_details.display_customer_5()
+Price_details.display_customer_6()
+Price_details.display_customer_7()
+Price_details.display_customer_8()
+Price_details.display_customer_9()
 print()
 print("===================MULTIPLE INHERITANCE========================")
 print()
@@ -83,40 +83,40 @@ print()
 Multilevel Inheritance
 
 '''
-class Cus_1:
-    def Apartment_flat1(self):
-        print("===================CUSTOMER_1============================")
-        print("Priya bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat2(self):
-        print("Priya bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat3(self):
+class Apartment_1:
+    def display_customer_1(self):
+        print("===================APARTMENT_1============================")
+        print("Bindhu bought 1-BHK-APARTMENT with price of RS.30,00,000")
+    def display_customer_2(self):
+        print("Preethi bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_3(self):
         print("Priya bought 3-BHK-APARTMENT with price of RS.70,00,000")
-class Cus_2(Cus_1):
-    def Apartment_flat4(self):
-        print("===================CUSTOMER_2============================")
-        print("Sreya bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat5(self):
-        print("Sreya bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat6(self):
+class Apartment_2(Apartment_1):
+    def display_customer_4(self):
+        print("===================APARTMENT_2============================")
+        print("Sayesha bought 1-BHK-APARTMENT with price of RS.30,00,000")
+    def display_customer_5(self):
+        print("Sri bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_6(self):
         print("Sreya bought 3-BHK-APARTMENT with price of RS.70,00,000")
-class Cus_3(Cus_2):
-    def Apartment_flat7(self):
-        print("===================CUSTOMER_3============================")
-        print("Bala bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat8(self):
-        print("Bala bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat9(self):
+class Apartment_3(Apartment_2):
+    def display_customer_7(self):
+        print("===================APARTMENT_3============================")
+        print("pragan bought 1-BHK-APARTMENT with price of RS.30,00,000")
+    def display_customer_8(self):
+        print("Bindhu bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_9(self):
         print("Bala bought 3-BHK-APARTMENT with price of RS.70,00,000")
-Price_details=Cus_3()
-Price_details.Apartment_flat1()
-Price_details.Apartment_flat2()
-Price_details.Apartment_flat3()
-Price_details.Apartment_flat4()
-Price_details.Apartment_flat5()
-Price_details.Apartment_flat6()
-Price_details.Apartment_flat7()
-Price_details.Apartment_flat8()
-Price_details.Apartment_flat9()
+Price_details=Apartment_3()
+Price_details.display_customer_1()
+Price_details.display_customer_2()
+Price_details.display_customer_3()
+Price_details.display_customer_4()
+Price_details.display_customer_5()
+Price_details.display_customer_6()
+Price_details.display_customer_7()
+Price_details.display_customer_8()
+Price_details.display_customer_9()
 print()
 print("===================MULTILEVEL INHERITANCE========================")
 print()
@@ -125,41 +125,41 @@ print()
 Hierarchical Inheritance
 
 '''
-class Cus_1:
-    def Apartment_flat1(self):
-        print("===================CUSTOMER_1============================")
-        print("Priya bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat2(self):
-        print("Priya bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat3(self):
+class Apartment_1:
+    def display_customer_1(self):
+        print("===================APARTMENT_1============================")
+        print("Preethi bought 1-BHK-APARTMENT with price of RS.30,00,000")
+    def display_customer_2(self):
+        print("sreya bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_3(self):
         print("Priya bought 3-BHK-APARTMENT with price of RS.70,00,000")
-class Cus_2(Cus_1):
-    def Apartment_flat4(self):
-        print("===================CUSTOMER_2============================")
-        print("Sreya bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat5(self):
-        print("Sreya bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat6(self):
+class Apartment_2(Apartment_1):
+    def display_customer_4(self):
+        print("===================APARTMENT_2============================")
+        print("Sri bought 1-BHK-APARTMENT with price of RS.30,00,000")
+    def display_customer_5(self):
+        print("Sai bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_6(self):
         print("Sreya bought 3-BHK-APARTMENT with price of RS.70,00,000")
-class Cus_3(Cus_1):
-    def Apartment_flat7(self):
-        print("===================CUSTOMER_3============================")
-        print("Bala bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat8(self):
-        print("Bala bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat9(self):
+class Apartment_3(Apartment_1):
+    def display_customer_7(self):
+        print("===================APARTMENT_3============================")
+        print("priya bought 1-BHK-APARTMENT with price of RS.30,00,000")
+    def display_customer_8(self):
+        print("Bindhu bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_9(self):
         print("Bala bought 3-BHK-APARTMENT with price of RS.70,00,000")
-Price_details_cus2=Cus_2()
-Price_details_cus3=Cus_3()
-Price_details_cus2.Apartment_flat1()
-Price_details_cus2.Apartment_flat2()
-Price_details_cus2.Apartment_flat3()
-Price_details_cus2.Apartment_flat4()
-Price_details_cus2.Apartment_flat5()
-Price_details_cus2.Apartment_flat6()
-Price_details_cus3.Apartment_flat7()
-Price_details_cus3.Apartment_flat8()
-Price_details_cus3.Apartment_flat9()
+Price_details1=Apartment_2()
+Price_details2=Apartment_3()
+Price_details1.display_customer_1()
+Price_details1.display_customer_2()
+Price_details1.display_customer_3()
+Price_details1.display_customer_4()
+Price_details1.display_customer_5()
+Price_details1.display_customer_6()
+Price_details2.display_customer_7()
+Price_details2.display_customer_8()
+Price_details2.display_customer_9()
 print()
 print("=================HIERARCHICAL INHERITANCE======================")
 print()
@@ -168,40 +168,40 @@ print()
 Hybrid Inheritance
 
 '''
-class Cus_1:
-    def Apartment_flat1(self):
-        print("===================CUSTOMER_1============================")
+class Apartment_1:
+    def display_customer_1(self):
+        print("===================APARTMENT_1============================")
         print("Priya bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat2(self):
-        print("Priya bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat3(self):
-        print("Priya bought 3-BHK-APARTMENT with price of RS.70,00,000")
-class Cus_2(Cus_1):
-    def Apartment_flat4(self):
-        print("===================CUSTOMER_2============================")
+    def display_customer_2(self):
+        print("Preethi bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_3(self):
+        print("sayesha bought 3-BHK-APARTMENT with price of RS.70,00,000")
+class Apartment_2(Apartment_1):
+    def display_customer_4(self):
+        print("===================APARTMENT_2============================")
         print("Sreya bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat5(self):
-        print("Sreya bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat6(self):
-        print("Sreya bought 3-BHK-APARTMENT with price of RS.70,00,000")
-class Cus_3(Cus_2,Cus_1):
-    def Apartment_flat7(self):
-        print("===================CUSTOMER_3============================")
+    def display_customer_5(self):
+        print("Sibin bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_6(self):
+        print("Sara bought 3-BHK-APARTMENT with price of RS.70,00,000")
+class Apartment_3(Apartment_2,Apartment_1):
+    def display_customer_7(self):
+        print("===================APARTMENT_3============================")
         print("Bala bought 1-BHK-APARTMENT with price of RS.30,00,000")
-    def Apartment_flat8(self):
-        print("Bala bought 2-BHK-APARTMENT with price of RS.50,00,000")
-    def Apartment_flat9(self):
-        print("Bala bought 3-BHK-APARTMENT with price of RS.70,00,000")
-Price_details_cus3=Cus_3()
-Price_details_cus3.Apartment_flat1()
-Price_details_cus3.Apartment_flat2()
-Price_details_cus3.Apartment_flat3()
-Price_details_cus3.Apartment_flat4()
-Price_details_cus3.Apartment_flat5()
-Price_details_cus3.Apartment_flat6()
-Price_details_cus3.Apartment_flat7()
-Price_details_cus3.Apartment_flat8()
-Price_details_cus3.Apartment_flat9()
+    def display_customer_8(self):
+        print("Bindhu bought 2-BHK-APARTMENT with price of RS.50,00,000")
+    def display_customer_9(self):
+        print("karthik bought 3-BHK-APARTMENT with price of RS.70,00,000")
+Price_details=Apartment_3()
+Price_details.display_customer_1()
+Price_details.display_customer_2()
+Price_details.display_customer_3()
+Price_details.display_customer_4()
+Price_details.display_customer_5()
+Price_details.display_customer_6()
+Price_details.display_customer_7()
+Price_details.display_customer_8()
+Price_details.display_customer_9()
 print()
 print("===================HYBRID INHERITANCE========================")
 print()
